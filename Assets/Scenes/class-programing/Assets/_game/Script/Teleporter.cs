0eab1653d3df3d9d5a10f5d7626039fdd63fec6b
DuongPham3456 <1phambacdaiduong2005@gmail.com>
2026-06-20 03:47:55 +0700

    map va tele

--- Assets/Scenes/class-programing/Assets/_game/Script/Teleporter.cs
+++ Assets/Scenes/class-programing/Assets/_game/Script/Teleporter.cs
@@ -1,18 +1,65 @@
 using UnityEngine;
+using TMPro; 
 
-public class Teleporter : MonoBehaviour
+public class Teleport : MonoBehaviour
 {
-    [Header("Điểm nhân vật sẽ tới")]
     public Transform destination; 
+    public string tenDiaDiem;     
+    public GameObject nutE;       
+    public TMP_Text textMesh;     
 
-    // Hàm này tự động chạy khi nhân vật dẫm vào khu vực cửa
+    private bool isPlayerInZone = false;
+    private GameObject player;
+
+    void Start()
+    {
+        // 1. Tự động ghép nối chữ
+        if (textMesh != null)
+        {
+            textMesh.text = "Bấm E để tới " + tenDiaDiem;
+        }
+
+        // 2. BẢO HIỂM LỖI: Ép dòng chữ phải tắt đi ngay khi game vừa bật lên!
+        if (nutE != null)
+        {
+            nutE.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (isPlayerInZone && Input.GetKeyDown(KeyCode.E))
+        {
+            // Dịch chuyển nhân vật
+            player.transform.position = destination.position;
+            
+            // Xóa chữ đi và reset trạng thái vùng để tránh lỗi
+            isPlayerInZone = false; 
+            if (nutE != null) nutE.SetActive(false);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            // Đổi vị trí của Player sang vị trí Điểm đến
-            other.transform.position = destination.position;
-            Debug.Log("Vù ù ù... Đã dịch chuyển!");
+            isPlayerInZone = true;
+            player = other.gameObject;
+            
+            // Hiện chữ lên khi đi vào vùng
+            if (nutE != null) nutE.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInZone = false;
+            player = null;
+            
+            // Tắt chữ đi khi đi ra khỏi vùng
+            if (nutE != null) nutE.SetActive(false);
         }
     }
 }
